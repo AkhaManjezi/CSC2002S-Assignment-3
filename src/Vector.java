2a1f7f21684db92fb005b2c191840001efc8bfb8
Akha Manjezi <amanjezi@gmail.com>
2019-09-04 18:08:46 +0200

    added count value

--- src/Vector.java
+++ src/Vector.java
@@ -1,20 +1,24 @@
 public class Vector {
     float x;
     float y;
+    int n;
 
     public Vector() {
         x =0;
         y=0;
+        n=0;
     }
 
-    public Vector(float x, float y) {
+    public Vector(float x, float y, int n) {
         this.x = x;
         this.y = y;
+        n = n;
     }
 
     void toAdd(Vector toAdd){
         x += toAdd.x;
         y += toAdd.y;
+        n++;
     }
 
     float mag(){
@@ -22,7 +26,12 @@ public class Vector {
     }
 
     Vector combine(Vector toCombine){
-        return new Vector(x+toCombine.x, y+toCombine.y);
+        Vector created = new Vector(x+toCombine.x, y+toCombine.y, n+toCombine.n);
+        return created;
+    }
+
+    Vector getAverage(){
+        return new Vector(x/n, y/n,1);
     }
 
     @Override
