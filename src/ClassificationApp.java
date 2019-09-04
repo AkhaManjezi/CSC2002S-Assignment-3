a132a86990825919a40bb072edcc4af863bd02e3
Akha Manjezi <amanjezi@gmail.com>
2019-09-04 18:08:12 +0200

    merged classification and avg into one method

--- src/ClassificationApp.java
+++ src/ClassificationApp.java
@@ -25,9 +25,12 @@ public class ClassificationApp {
     public static void main(String[] args) {
         getData();
 //        tick();
-        prevWindlin();
-        localWindlin();
+//        prevWindlin();
+//        localWindlin();
 //        float time = tock();
+
+        sequential();
+
 //        System.out.println(time);
 //        tick();
 //        prevWindFJ();
@@ -43,25 +46,174 @@ public class ClassificationApp {
 
     static void prevWindFJ(){
         Vector wind = sum2(data.linAdvection);
-//        wind.x = (wind.x/data.dim());
-//        wind.y = (wind.y/data.dim());
-//        System.out.println(wind.x);
-//        System.out.println(wind.y);
-    }
-
-    static void prevWindlin(){
-        Vector wind = new Vector(0,0);
-        for (int i = 0; i < data.dimt; i++) {
-            for (int j = 0; j < data.dimx; j++) {
-                for (int k = 0; k < data.dimy; k++) {
-                    wind.toAdd(data.advection[i][j][k]);
-                }
-            }
-        }
-        System.out.println(String.format("%s %s %s", data.dimt, data.dimx, data.dimy));
         wind.x = (wind.x/data.dim());
         wind.y = (wind.y/data.dim());
-        System.out.println(wind.toString());
+        System.out.println(wind.x);
+        System.out.println(wind.y);
+        
+    }
+
+    static void sequential(){
+        Vector winds = new Vector();
+        int classcount = 0;
+        int at = 0;
+        int left = -3;
+        int right = 3;
+        int above = -data.dimx*3;
+        int bottom = data.dimx*3;
+        int x, y, u;
+        boolean aboveleft, aboveright, bottomleft,bottomright;
+        Vector wind2 = new Vector(0,0,0);
+        for (int i = 0; i < data.linAdvection.length; i+=3) {
+            wind2.toAdd(new Vector(data.linAdvection[i], data.linAdvection[i+1],1));
+
+            x = i;
+            y = i + 1;
+            u = i + 2;
+            aboveleft = false;
+            aboveright = false;
+            bottomleft = false;
+            bottomright = false;
+
+            winds.x += data.linAdvection[x];
+            winds.y += data.linAdvection[y];
+            winds.n++;
+
+            if(at%data.dimx!=0){
+                //add left
+                winds.x += data.linAdvection[x+left];
+                winds.y += data.linAdvection[y+left];
+                winds.n++;
+                if(at >= data.dimx){
+                    //add above left
+                    winds.x += data.linAdvection[x+left+above];
+                    winds.y += data.linAdvection[y+left+above];
+                    winds.n++;
+                    aboveleft = true;
+                }
+                if((at+data.dimx)<data.dimx*data.dimy){
+                    //add bottom left
+                    winds.x += data.linAdvection[x+left+bottom];
+                    winds.y += data.linAdvection[y+left+bottom];
+                    winds.n++;
+                    bottomleft = true;
+                }
+            }
+            if(at >= data.dimx){
+                //add above
+                winds.x += data.linAdvection[x+above];
+                winds.y += data.linAdvection[y+above];
+                winds.n++;
+                if(at%data.dimx!=0 && aboveleft == false) {
+                    //add above left
+                    winds.x += data.linAdvection[x+above+left];
+                    winds.y += data.linAdvection[y+above+left];
+                    winds.n++;
+                }
+                if((at+1)%data.dimx!=0) {
+                    //add above right
+                    winds.x += data.linAdvection[x+above + right];
+                    winds.y += data.linAdvection[y+above + right];
+                    winds.n++;
+                    aboveright = true;
+                }
+            }
+            if((at+1)%data.dimx!=0){
+                //add right
+                winds.x += data.linAdvection[x+right];
+                winds.y += data.linAdvection[y+right];
+                winds.n++;
+                if((at+data.dimx)<data.dimx*data.dimy){
+                    //add bottom right
+                    winds.x += data.linAdvection[x+right+bottom];
+                    winds.y += data.linAdvection[y+right+bottom];
+                    winds.n++;
+                    bottomright=true;
+                }
+                if(at >= data.dimx && aboveright==false){
+                    //add above right
+                    winds.x += data.linAdvection[x+above+right];
+                    winds.y += data.linAdvection[y+above+right];
+                    winds.n++;
+                }
+            }
+            if((at+data.dimx)<data.dimx*data.dimy){
+                //add bottom
+                winds.x += data.linAdvection[x+bottom];
+                winds.y += data.linAdvection[y+bottom];
+                winds.n++;
+                if(at%data.dimx!=0 && bottomleft==false) {
+                    //add bottom left
+                    winds.x += data.linAdvection[x+bottom+left];
+                    winds.y += data.linAdvection[y+bottom+left];
+                    winds.n++;
+                }
+                if((at+1)%data.dimx!=0 && bottomright==false) {
+                    //add bottom right
+                    winds.x += data.linAdvection[x+bottom+right];
+                    winds.y += data.linAdvection[y+bottom+right];
+                    winds.n++;
+                }
+            }
+            at+=1;
+            if(at == data.dimx*data.dimy){
+                at = 0;
+            }
+            
+            Vector wind = winds.getAverage();
+
+            if (Math.abs(data.linAdvection[u]) > wind.mag()){
+                data.linClassification[classcount++] = 0;
+            }else if(wind.mag() > 0.2){
+                data.linClassification[classcount++] = 1;
+            }else{
+                data.linClassification[classcount++] = 2;
+            }
+
+            winds.n=0;
+            winds.x = 0;
+            winds.y = 0;
+
+        }
+        System.out.println(String.format("%s %s %s", data.dimt, data.dimx, data.dimy));
+        wind2 = wind2.getAverage();
+        System.out.println(wind2.toString());
+        String output2 = "";
+        int count3 = 1;
+        for (int i = 0; i < data.linClassification.length; i++) {
+            output2 += data.linClassification[i] + "";
+            if(count3 < data.dimx*data.dimy){
+                output2 += " ";
+            }else{
+                output2 += "\n";
+                count3=0;
+            }
+            count3++;
+        }
+        System.out.println(output2);
+    }
+    
+
+    static void prevWindlin(){
+//        Vector wind = new Vector(0,0);
+//        for (int i = 0; i < data.dimt; i++) {
+//            for (int j = 0; j < data.dimx; j++) {
+//                for (int k = 0; k < data.dimy; k++) {
+//                    wind.toAdd(data.advection[i][j][k]);
+//                }
+//            }
+//        }
+//        wind.x = (wind.x/data.dim());
+//        wind.y = (wind.y/data.dim());
+//        System.out.println(wind.toString());
+        Vector wind2 = new Vector(0,0,0);
+        for (int i = 0; i < data.linAdvection.length; i+=3) {
+            wind2.toAdd(new Vector(data.linAdvection[i], data.linAdvection[i+1],1));
+        }
+        System.out.println(String.format("%s %s %s", data.dimt, data.dimx, data.dimy));
+        wind2 = wind2.getAverage();
+        System.out.println(wind2.toString());
+        
 //        System.out.println(wind.x);
 //        System.out.println(wind.y);
     }
@@ -227,7 +379,7 @@ public class ClassificationApp {
 
             winds[count] = winds[count]/neighbours;
             winds[count+1] = winds[count+1]/neighbours;
-            Vector wind = new Vector(winds[count], winds[count+1]);
+            Vector wind = new Vector(winds[count], winds[count+1], 1);
 
             if (Math.abs(data.linAdvection[u]) > wind.mag()){
                 data.linClassification[classcount++] = 0;
