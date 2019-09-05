ace9057429ae71795939ce6d6611019f938fc9cc
Akha Manjezi <amanjezi@gmail.com>
2019-09-05 17:22:03 +0200

    created object to hold Recursive version output

---
+++ src/OutputObject.java
@@ -0,0 +1,9 @@
+public class OutputObject {
+    CloudData data;
+    Vector wind;
+
+    public OutputObject(CloudData data, Vector wind) {
+        this.data = data;
+        this.wind = wind;
+    }
+}
